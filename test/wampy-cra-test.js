7cbbf53615da55cd2f93ab2aee4e9389c865cc22
Konstantin Burkalev <KSDaemon@yandex.ru>
2016-05-19 15:22:27 +0300

    Added travis and some tests

---
+++ test/wampy-cra-test.js
@@ -0,0 +1,76 @@
+/**
+ * Created by KSDaemon on 19.05.16.
+ */
+
+var expect = require('chai').expect,
+    wampyCra = require('./../src/wampy-cra'),
+    secret = 'secretKey',
+    salt = 'secretSalt',
+    challenge = '{"authrole": "commonuser", ' +
+        '"authprovider": "static", ' +
+        '"authmethod": "wampcra", ' +
+        '"timestamp": "2016-05-19T09:30:32.177Z", ' +
+        '"session": 6492709588079659, ' +
+        '"authid": "joe", ' +
+        '"nonce": "Z+UXH9Q8htepjpDYGAuwBQgeQTSX2W9eZrEJLFrr1ohVJVHjzIaxgw7niL6CsAQb"}';
+
+describe('Wampy-cra plugin', function () {
+
+    it('allows to sign challenge message with secret key using SHA256', function () {
+        var res = wampyCra.sign(secret, challenge);
+
+        expect(res).to.be.equal('pKj6AlpNYn7QKqAZD0WctTVO+nxpSG1kT9muLzbv5Mo=');
+    });
+
+    it('allows to generate derived key using PBKDF2 scheme', function () {
+        var res;
+
+        res = wampyCra.derive_key(secret, salt);
+        expect(res).to.be.equal('PjhbMq82TzEZgl7/qKoUrk2ZdYl3sBciwBVSnE7ZCYM=');
+
+        res = wampyCra.derive_key(secret, salt, 500);
+        expect(res).to.be.equal('t/+iDiMZLt/mFSzbtq7vJHdExA3FtBu0LukzpTFYBpI=');
+
+        res = wampyCra.derive_key(secret, salt, 2000, 16);
+        expect(res).to.be.equal('ER8wnMRXtdNgY8tJ37FkhQ==');
+    });
+
+    it('allows to automatically detect requested WAMP auth method and produce signed message', function () {
+        var res,
+            auto = wampyCra.auto(secret),
+            challengeInfo = {
+                challenge : challenge,
+                iterations: 100,
+                salt      : salt,
+                keylen    : 16
+            };
+
+        res = auto('wampcra', challengeInfo);
+        expect(res).to.be.equal('W+wnbw5Uo9rkDIrQf1SSJue4JPzkqWQE89sO4qndUs4=');
+
+        challengeInfo = {
+            challenge : challenge
+        };
+
+        res = auto('wampcra', challengeInfo);
+        expect(res).to.be.equal('pKj6AlpNYn7QKqAZD0WctTVO+nxpSG1kT9muLzbv5Mo=');
+    });
+
+    it('throws error while using auto method, if auth method is not recognized', function () {
+        var res,
+            auto = wampyCra.auto(secret),
+            challengeInfo = {
+                challenge : challenge,
+                iterations: 100,
+                salt      : salt,
+                keylen    : 16
+            };
+
+        try {
+            res = auto('notwampcra', challengeInfo);
+        } catch (e) {
+            expect(e.message).to.be.equal('Unknown authentication method requested!');
+        }
+    });
+
+});
