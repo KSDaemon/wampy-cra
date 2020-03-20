a5daf0f90adf0d77e68832a7bc9ee6c757d8d520
Konstantin Burkalev <KSDaemon@ya.ru>
2020-03-20 13:35:56 +0200

    Created build version

--- build/wampy-cra.js
+++ build/wampy-cra.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && _typeof(Symbol.iterator) === "symbol") { _typeof = function (_typeof2) { function _typeof(_x3) { return _typeof2.apply(this, arguments); } _typeof.toString = function () { return _typeof2.toString(); }; return _typeof; }(function (obj) { return typeof obj === "undefined" ? "undefined" : _typeof(obj); }); } else { _typeof = function (_typeof3) { function _typeof(_x4) { return _typeof3.apply(this, arguments); } _typeof.toString = function () { return _typeof3.toString(); }; return _typeof; }(function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj === "undefined" ? "undefined" : _typeof(obj); }); } return _typeof(obj); }
 
 /**
  * Project: wampy-cra.js
@@ -13,71 +13,64 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
  * See @license text at http://www.opensource.org/licenses/mit-license.php
  *
  */
-
 // Module boilerplate to support browser globals and browserify and AMD.
 (typeof define === 'function' ? function (m) {
-    define('WampyCra', m);
-} : (typeof exports === 'undefined' ? 'undefined' : _typeof(exports)) === 'object' ? function (m) {
-    module.exports = m();
+  define('WampyCra', m);
+} : (typeof exports === "undefined" ? "undefined" : _typeof(exports)) === 'object' ? function (m) {
+  module.exports = m();
 } : function (m) {
-    this.WampyCra = m();
+  this.WampyCra = m();
 })(function () {
+  var WampyCra = {},
+      isNode = (typeof process === "undefined" ? "undefined" : _typeof(process)) === 'object' && Object.prototype.toString.call(process) === '[object process]',
+      crypto = isNode ? require('crypto') : require('crypto-js');
 
-    var WampyCra = {},
-        isNode = (typeof process === 'undefined' ? 'undefined' : _typeof(process)) === 'object' && Object.prototype.toString.call(process) === '[object process]',
-        crypto = isNode ? require('crypto') : require('crypto-js');
+  function derive_key(secret, salt) {
+    var iterations = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 1000;
+    var keylen = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 32;
+    var key = void 0;
 
-    function derive_key(secret, salt) {
-        var iterations = arguments.length <= 2 || arguments[2] === undefined ? 1000 : arguments[2];
-        var keylen = arguments.length <= 3 || arguments[3] === undefined ? 32 : arguments[3];
+    if (isNode) {
+      key = crypto.pbkdf2Sync(secret, salt, iterations, keylen, 'sha256');
+      return key.toString('base64');
+    } else {
+      var config = {
+        keySize: keylen / 4,
+        iterations: iterations,
+        hasher: crypto.algo.SHA256
+      };
+      key = crypto.PBKDF2(secret, salt, config);
+      return key.toString(crypto.enc.Base64);
+    }
+  }
 
-        var key = void 0;
+  function sign(key, challenge) {
+    if (isNode) {
+      var hmac = crypto.createHmac('sha256', key);
+      hmac.update(challenge);
+      return hmac.digest('base64');
+    } else {
+      return crypto.HmacSHA256(challenge, key).toString(crypto.enc.Base64);
+    }
+  }
 
-        if (isNode) {
-            key = crypto.pbkdf2Sync(secret, salt, iterations, keylen, 'sha256');
-            return key.toString('base64');
+  function auto(secret) {
+    return function (method, info) {
+      if (method === 'wampcra') {
+        if (info.salt) {
+          return sign(derive_key(secret, info.salt, info.iterations, info.keylen), info.challenge);
         } else {
-            var config = {
-                keySize: keylen / 4,
-                iterations: iterations,
-                hasher: crypto.algo.SHA256
-            };
-
-            key = crypto.PBKDF2(secret, salt, config);
-            return key.toString(crypto.enc.Base64);
+          return sign(secret, info.challenge);
         }
-    }
+      } else {
+        throw new Error('Unknown authentication method requested!');
+      }
+    };
+  }
 
-    function sign(key, challenge) {
-        if (isNode) {
-            var hmac = crypto.createHmac('sha256', key);
-            hmac.update(challenge);
-            return hmac.digest('base64');
-        } else {
-            return crypto.HmacSHA256(challenge, key).toString(crypto.enc.Base64);
-        }
-    }
-
-    function auto(secret) {
-
-        return function (method, info) {
-            if (method === 'wampcra') {
-
-                if (info.salt) {
-                    return sign(derive_key(secret, info.salt, info.iterations, info.keylen), info.challenge);
-                } else {
-                    return sign(secret, info.challenge);
-                }
-            } else {
-                throw new Error('Unknown authentication method requested!');
-            }
-        };
-    }
-
-    WampyCra.derive_key = derive_key;
-    WampyCra.sign = sign;
-    WampyCra.auto = auto;
-
-    return WampyCra;
+  WampyCra.derive_key = derive_key;
+  WampyCra.sign = sign;
+  WampyCra.auto = auto;
+  return WampyCra;
 });
 //# sourceMappingURL=wampy-cra.js.map
